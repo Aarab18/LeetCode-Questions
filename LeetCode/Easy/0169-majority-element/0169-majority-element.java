90ba4af84f18907af845ab6937b140883e7e9af8
Aarab Ahmad <150062569+Aarab18@users.noreply.github.com>
2025-09-03 03:16:10 +0530

    18 ms (13.24%) 50.5 MB (71.12%) 09-03-2025 0169-majority-element Easy .java

---
+++ LeetCode/Easy/0169-majority-element/0169-majority-element.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        if(nums.length==1) {
+            return nums[0];
+        }
+        Arrays.sort(nums);
+		System.out.println(Arrays.toString(nums));
+		int count=1;
+		int i=1;
+		while(i<nums.length) {
+			if(nums[i]==nums[i-1]) {
+				count++;
+			}
+			else {
+				count=1;
+			}
+			if(count>nums.length/2) {
+				return nums[i];
+			}
+			i++;
+		}
+        return 0;
+    }
+}
