0fd47a555b58d812d048c3486ac64182de0fe8fa
Aarab Ahmad <150062569+Aarab18@users.noreply.github.com>
2025-10-06 15:27:32 +0530

    2 ms (84.72%) 48.6 MB (24.28%) 10-06-2025 0143-reorder-list Medium .java

--- LeetCode/Medium/0143-reorder-list/0143-reorder-list.java
+++ LeetCode/Medium/0143-reorder-list/0143-reorder-list.java
@@ -1,44 +1,54 @@
-  public class Solution {
-    
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode findMid(ListNode head) {
+        ListNode slow=head;
+        ListNode fast=head.next;
+        while(fast!=null && fast.next!=null) {
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
+    }
+    public ListNode reverse(ListNode Node) {
+        ListNode prev=null;
+        ListNode curr=Node;
+        ListNode next;
+        while(curr!=null) {
+            next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+        return prev;
+    }
+    public void merge(ListNode l1, ListNode l2) {
+        while(l1!=null && l2!=null) {
+            ListNode n1=l1.next;
+            ListNode n2=l2.next;
+            l1.next=l2;
+            if(n1==null) {
+                break;
+            }
+            l2.next=n1;
+            l1=n1;
+            l2=n2;
+        }
+    }
     public void reorderList(ListNode head) {
-      if (head == null || head.next == null)
-          return;
-      ListNode prev = null, slow = head, fast = head, l1 = head;
-      while (fast != null && fast.next != null) {
-        prev = slow;
-        slow = slow.next;
-        fast = fast.next.next;
-      }
-      prev.next = null;
-      ListNode l2 = reverse(slow);
-      merge(l1, l2);
+        ListNode list1=head;
+        ListNode mid=findMid(head);
+        ListNode list2=mid.next;
+        mid.next=null;
+        list2=reverse(list2);
+        merge(list1, list2);
     }
-    
-    ListNode reverse(ListNode head) {
-      ListNode prev = null, curr = head, next = null;
-      
-      while (curr != null) {
-        next = curr.next;
-        curr.next = prev;
-        prev = curr;
-        curr = next;
-      }
-      
-      return prev;
-    }
-    
-    void merge(ListNode l1, ListNode l2) {
-      while (l1 != null) {
-        ListNode n1 = l1.next, n2 = l2.next;
-        l1.next = l2;
-        
-        if (n1 == null)
-          break;
-            
-        l2.next = n1;
-        l1 = n1;
-        l2 = n2;
-      }
-    }
-
-  }
+}
