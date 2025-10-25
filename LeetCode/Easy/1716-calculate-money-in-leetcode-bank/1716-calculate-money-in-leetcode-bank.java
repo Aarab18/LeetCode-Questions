eea1723897ebec869a7e771953a957f0a994e64a
Aarab Ahmad <150062569+Aarab18@users.noreply.github.com>
2025-10-25 21:32:01 +0530

    90 ms (2.16%) 43.9 MB (6.06%) 10-25-2025 1716-calculate-money-in-leetcode-bank Easy .java

---
+++ LeetCode/Easy/1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int totalMoney(int n) {
+        int sum=0, start=1, count=0, counts=0;
+        for(int i=0;i<n;i++) {
+            sum+=(start+i);
+            System.out.println(start+i);
+            count++;
+            counts++;
+            if(count==7) {
+                start++;
+                i=-1;
+                count=0;
+            }
+            if(counts>=n) {
+                return sum;
+            }
+        }
+        return sum;
+    }
+}
