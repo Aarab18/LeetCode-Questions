c2e00984d59b8d5b97c45fb652cc132c8ba34b11
Aarab Ahmad <150062569+Aarab18@users.noreply.github.com>
2025-10-27 23:28:49 +0530

    15 ms (10.57%) 45.8 MB (8.84%) 10-27-2025 2125-number-of-laser-beams-in-a-bank Medium .java

--- LeetCode/Medium/2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.java
+++ LeetCode/Medium/2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.java
@@ -1,25 +1,20 @@
 class Solution {
     public int numberOfBeams(String[] bank) {
-        List<Integer> ones=new ArrayList<>();
+        int prev=0;
+        int sum=0;
         for(String str : bank) {
-            int count=0;
+            int current=0;
             for(char ch : str.toCharArray()) {
                 if(ch=='1') {
-                    count++;
+                    current++;
                 }
             }
-            if(count!=0) {
-                ones.add(count);
+            if(current!=0) {
+                sum+=(current*prev);
+                prev=current;
             }
         }
-        System.out.println(ones);
-        if(ones.size()<=1) {
-            return 0;
-        }
-        int sum=0;
-        for(int i=0;i<ones.size()-1;i++) {
-            sum+=(ones.get(i)*ones.get(i+1));
-        }
+        System.out.println(sum);
         return sum;
     }
 }
