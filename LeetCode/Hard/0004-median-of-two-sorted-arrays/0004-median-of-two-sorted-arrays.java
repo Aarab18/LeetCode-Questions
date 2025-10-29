593764e9cfdcb9694b896e9850d382e8072c57b0
Aarab Ahmad <150062569+Aarab18@users.noreply.github.com>
2025-10-29 23:37:39 +0530

    11 ms (5.76%) 46.3 MB (28.48%) 10-29-2025 0004-median-of-two-sorted-arrays Hard .java

--- LeetCode/Hard/0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ LeetCode/Hard/0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,26 +1,34 @@
 class Solution {
-    public int findIndex(List <Integer> nums, int val) {
-        int index=Collections.binarySearch(nums, val);
-        if(index<0) {
-            index=-index-1;
+    public int[] merge(int[] nums1, int[] nums2) {
+        int res[]=new int[nums1.length+nums2.length];
+        int index=0;
+        int i=0,j=0;
+        while(i<nums1.length && j<nums2.length) {
+            if((i<nums1.length && j<nums2.length) && nums1[i]>=nums2[j]) {
+                res[index++]=nums2[j++];
+            }
+            if((i<nums1.length && j<nums2.length) && nums2[j]>nums1[i]) {
+                res[index++]=nums1[i++];
+            }
         }
-        return index;
+        while(i<nums1.length) {
+            res[index++]=nums1[i++];
+        }
+        while(j<nums2.length) {
+            res[index++]=nums2[j++];
+        }
+        return res;
     }
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        List<Integer> merged=new ArrayList<>();
-        for(int i: nums1) {
-            merged.add(i);
+        int[] res=new int[nums1.length+nums2.length];
+        res=merge(nums1, nums2);
+        System.out.println(Arrays.toString(res));
+        if(res.length%2==0) {
+            double num1=res[res.length/2]+res[res.length/2-1];
+            num1=num1/2;
+            return num1;
         }
-        for(int i=0;i<nums2.length;i++) {
-            int index=findIndex(merged, nums2[i]);
-            merged.add(index, nums2[i]);
-        }
-        double res=0;
-        int len=merged.size()/2;
-        if(merged.size()%2==0) {
-            res=((double)merged.get(len)+(double)merged.get(len-1))/2;
-            return res;
-        }
-        return (double)merged.get(len);
+        double num2=res[res.length/2];
+        return num2;
     }
 }
