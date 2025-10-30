877965bfafebf2f2d5d8e2f43a81f34b2570f07c
Aarab Ahmad <150062569+Aarab18@users.noreply.github.com>
2025-10-30 17:21:09 +0530

    51 ms (5.03%) 57.6 MB (6.8%) 10-30-2025 1561-maximum-number-of-coins-you-can-get Medium .java

---
+++ LeetCode/Medium/1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.java
@@ -0,0 +1,16 @@
+import java.util.stream.*;
+class Solution {
+    public int maxCoins(int[] piles) {
+        Arrays.sort(piles);
+        int n=piles.length;
+        int self=n-2;
+        Set<Integer> set=new HashSet<>();
+        int mySum=0;
+        System.out.println(Arrays.toString(piles));
+        for(int i=0;i<n/3;i++) {
+            mySum+=piles[self];
+            self-=2;
+        }
+        return mySum;
+    }
+}
