62a16c52d11e2862a0022d05d8577405282b4c0e
Aarab Ahmad <150062569+Aarab18@users.noreply.github.com>
2025-11-15 00:42:09 +0530

    304 ms (5.08%) 105.7 MB (5.05%) 11-15-2025 0128-longest-consecutive-sequence Medium .java

---
+++ LeetCode/Medium/0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        if(nums.length==0) {
+            return 0;
+        }
+        Map<Integer, Integer> map=new TreeMap<>();
+        for(int num : nums) {
+            map.put(num, map.getOrDefault(num, 0)+1);
+        }
+        System.out.println(map);
+        List<Integer> keys = new ArrayList<>(map.keySet());
+        System.out.println(keys);
+        int max=1, count=1;;
+        for(int i=0;i<keys.size()-1;i++) {
+            if(keys.get(i)+1==keys.get(i+1)) {
+                count++;
+            }
+            max=Math.max(count, max);
+            if(keys.get(i)+1!=keys.get(i+1)) {
+                count=1;
+            }
+        }
+        return max;
+    }
+}
