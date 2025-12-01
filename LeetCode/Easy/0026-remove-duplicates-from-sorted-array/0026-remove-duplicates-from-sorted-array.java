ac98a027703bcf95f58675d4b11931588bfba68a
Aarab Ahmad <150062569+aarabahmad@users.noreply.github.com>
2025-12-01 10:48:27 +0530

    1 ms (64.25%) 46.8 MB (7.04%) 12-01-2025 0026-remove-duplicates-from-sorted-array Easy .java

--- LeetCode/Easy/0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ LeetCode/Easy/0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,15 +1,10 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        Map<Integer, Integer> map=new LinkedHashMap<>();
-        for(int num : nums) {
-            if(!map.containsKey(num)) {
-                map.put(num, 1);
+        int index=1;
+        for(int i=1;i<nums.length;i++) {
+            if(nums[i]!=nums[i-1]) {
+                nums[index++]=nums[i];
             }
-        }
-        System.out.println(map);
-        int index=0;
-        for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            nums[index++]=entry.getKey();
         }
         return index;
     }
