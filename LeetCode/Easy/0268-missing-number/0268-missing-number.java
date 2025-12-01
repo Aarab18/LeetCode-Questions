e6ce6755c5c6e3006c87951d3d3f3ca372eebae8
Aarab Ahmad <150062569+aarabahmad@users.noreply.github.com>
2025-12-01 15:23:15 +0530

    0 ms (100%) 47.2 MB (27.11%) 12-01-2025 0268-missing-number Easy .java

--- LeetCode/Easy/0268-missing-number/0268-missing-number.java
+++ LeetCode/Easy/0268-missing-number/0268-missing-number.java
@@ -1,11 +1,11 @@
 class Solution {
     public int missingNumber(int[] nums) {
         int n=nums.length;
-        int totSum=(n*(n+1))/2;
-        int sum=0;
-        for(int num : nums)  {
-            sum+=num;
+        int actualSum=(n*(n+1))/2;
+        int observedSum=0;
+        for(int num : nums) {
+            observedSum+=num;
         }
-        return totSum-sum;
+        return actualSum-observedSum;
     }
 }
