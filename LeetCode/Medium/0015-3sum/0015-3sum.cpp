772f2e1d7a0e07777da765f9bb4875a5b76ebcd3
Aarab Ahmad <150062569+aarabahmad@users.noreply.github.com>
2025-12-13 13:53:14 +0530

    41 ms (87.84%) 29 MB (89.73%) 12-13-2025 0015-3sum Medium .cpp

---
+++ LeetCode/Medium/0015-3sum/0015-3sum.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> triplets;
+        for(int i=0;i<nums.size();i++){
+            if(i>0 && nums[i]==nums[i-1]){
+                continue;
+            }
+            int target=-nums[i];
+            int j=i+1, k=nums.size()-1;
+            while(j<k){
+                if((nums[j]+nums[k])==target){
+                    triplets.push_back({nums[i], nums[j], nums[k]});
+                    while (j<k && nums[j]==nums[j+1]){
+                        j++;
+                    }
+                    while (j<k && nums[k]==nums[k-1]){
+                        k--;
+                    }
+                    j++;
+                    k--;
+                }
+                else if((nums[j]+nums[k])<target){
+                    j++;
+                }
+                else{
+                    k--;
+                }
+            }
+        }
+        return triplets;
+    }
+};
