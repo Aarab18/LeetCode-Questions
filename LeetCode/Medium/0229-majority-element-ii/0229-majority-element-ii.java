cdfa5d8e46cabb1abdf7262befe32aa3ebdf9fc0
Aarab Ahmad <150062569+aarabahmad@users.noreply.github.com>
2025-12-13 13:20:57 +0530

    2 ms (99.8%) 52.6 MB (30.36%) 12-13-2025 0229-majority-element-ii Medium .java

--- LeetCode/Medium/0229-majority-element-ii/0229-majority-element-ii.java
+++ LeetCode/Medium/0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,17 +1,43 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        Map<Integer, Integer> map=new HashMap<>();
-        Set<Integer> set=new HashSet<>();
-        List<Integer> majority=new ArrayList<>();
+        int count1=0, count2=0;
+        int val1=Integer.MIN_VALUE, val2=Integer.MIN_VALUE;
         for(int num : nums) {
-            map.put(num, map.getOrDefault(num, 0)+1);
-            if(map.get(num)>nums.length/3) {
-                set.add(num);
+            if(count1==0 && val2!=num) {
+                count1=1;
+                val1=num;
+            }
+            else if(count2==0 && val1!=num) {
+                count2=1;
+                val2=num;
+            }
+            else if(val1==num) {
+                count1++;
+            }
+            else if(val2==num) {
+                count2++;
+            }
+            else {
+                count1--;
+                count2--;
             }
         }
-        for(int num : set) {
-            majority.add(num);
+        count1=0; count2=0;
+        for(int num : nums) {
+            if(num==val1) {
+                count1++;
+            }
+            if(num==val2) {
+                count2++;
+            }
         }
-        return majority;
+        List<Integer> list=new ArrayList<>();
+        if(count1>nums.length/3) {
+            list.add(val1);
+        }
+        if(count2>nums.length/3) {
+            list.add(val2);
+        }
+        return list;
     }
 }
